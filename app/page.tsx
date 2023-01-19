0984dbfaddc81ed261246eb1ded27ac3ad12cdd2
Felix Mokross <felix.mokross@zuehlke.com>
2023-01-19 22:04:23 +0100

    Add Inter font integrated with Tailwind CSS

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,3 @@
-import { Inter } from "@next/font/google";
-
-// TODO what to do with this?
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} \*/
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
