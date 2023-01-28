ffdfb2c4887b224d76e98c05d81d50190d279a0a
Felix Mokross <felix.mokross@zuehlke.com>
2023-01-28 20:34:36 +0100

    Add prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Welcome to Worklog</h1>;
+import { PropsWithChildren } from "react";
+import prisma from "./common/prisma";
+
+export default async function Home() {
+  const x = await prisma.mandate.findMany();
+  return (
+    <>
+      <WeekView />
+      {/* <h1 className="text-3xl font-bold underline">Welcome to Worklog</h1>
+      {x.map((mandate) => (
+        <p key={mandate.id}>{mandate.name}</p>
+      ))} */}
+    </>
+  );
+}
+
+function WeekView() {
+  return (
+    <div>
+      <DayColumn day="Mon" />
+      <DayColumn day="Tue" />
+      <DayColumn day="Wed" />
+      <DayColumn day="Thu" />
+      <DayColumn day="Fri" />
+      <DayColumn day="Sat" />
+      <DayColumn day="Sun" />
+    </div>
+  );
+}
+
+type DayColumnProps = { day: string };
+
+function DayColumn({ day }: DayColumnProps) {
+  return <div>{day}</div>;
 }
